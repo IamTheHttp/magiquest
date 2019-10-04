0c1ca1420e1011fff2579c3ecb05083fc44b651d
Patrick Tolosa <patrick.tolosa@ladbrokescoral.com>
2019-10-04 20:49:35 +0300

    moved the magical number 32 to a config

---
+++ src/gameEngine/config.js
@@ -0,0 +1,10 @@
+export const CIRCLE_SHAPE = 'CIRCLE_SHAPE';
+export const CANVAS_OUTPUT = 'CANVAS_OUTPUT';
+export const MAP_TILE_SHAPE = 'MAP_TILE_SHAPE';
+export const RECT_SHAPE = 'RECT_SHAPE';
+export const MOVE_ACTION = 'MOVE_ACTION';
+
+
+
+
+
