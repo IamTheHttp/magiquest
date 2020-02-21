91d0afcbff53ae47256cfa22d4e507716585b3e2
Patrick <patrick.tolosa@gmail.com>
2020-02-21 15:19:03 +0200

    Build the project to update /dist files so to allow testing in production mode

--- dist/gameEngine/components/AIControlledComp.js
+++
@@ -1,8 +0,0 @@
-import { AI_CONTROLLED_COMP } from './ComponentNamesConfig';
-var AIControlledComp = /** @class */ (function () {
-    function AIControlledComp() {
-        this.name = AI_CONTROLLED_COMP;
-    }
-    return AIControlledComp;
-}());
-export default AIControlledComp;
--- dist/gameEngine/components/PlayerControlledComponent.js
+++
@@ -1,8 +0,0 @@
-import { PLAYER_CONTROLLED_COMP } from './ComponentNamesConfig';
-var PlayerControlledComponent = /** @class */ (function () {
-    function PlayerControlledComponent() {
-        this.name = PLAYER_CONTROLLED_COMP;
-    }
-    return PlayerControlledComponent;
-}());
-export default PlayerControlledComponent;
