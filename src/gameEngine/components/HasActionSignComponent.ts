ac9dd297db480027a8c3eb456d0dc577f262327b
Patrick <patrick.tolosa@gmail.com>
2020-03-13 22:55:06 +0200

    Add component HAS_ACTION_SIGN_COMP

---
+++ src/gameEngine/components/HasActionSignComponent.ts
@@ -0,0 +1,10 @@
+import {HAS_ACTION_SIGN_COMP} from './ComponentNamesConfig';
+
+class HasActionSignComponent {
+  name: string;
+  constructor() {
+    this.name = HAS_ACTION_SIGN_COMP;
+  }
+}
+
+export default HasActionSignComponent;
