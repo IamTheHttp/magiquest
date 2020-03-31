3ad735524051de74c16e444001537ff14dc41f8f
Patrick <patrick.tolosa@gmail.com>
2020-03-31 05:48:30 +0300

    Temporary fix a bug related to dead entities being moved around

--- src/gameEngine/utils/systemUtils/move/updateMapTileIdx.ts
+++ src/gameEngine/utils/systemUtils/move/updateMapTileIdx.ts
@@ -1,6 +1,7 @@
 import {getTileIdxByPos} from '../../componentUtils/tileUtils/getTileIdx';
 import BaseEntity from "BaseEntity";
 import {ITileIndexMap} from "../../../../interfaces/interfaces";
+import {HEALTH_COMP} from "components/ComponentNamesConfig";
 
 interface updateMapTileIdxArguments {
   entity: BaseEntity,
@@ -12,12 +13,21 @@ interface updateMapTileIdxArguments {
 }
 
 function updateMapTileIdx({entity, tileIdxMap,  oldX = null, oldY = null, newX = null, newY = null}: updateMapTileIdxArguments) {
+  // This was due to a bug that caused dead entities
+  // that on the next tick needed to change tiles
+  // to still move after being dead (since death is async by a tick.
+  // TODO all systems actually operate on dead entities, they can still attack for example, this needs to be addressed
+  // TODO - A dead Entity should probably not survive the tick...
+
+  if (entity.hasComponents(HEALTH_COMP) && entity[HEALTH_COMP].current === 0) {
+    return;
+  }
   /**
    * @type IndexedTile
    */
   let oldIndexedTile = tileIdxMap[getTileIdxByPos(oldX, oldY)];
   let newIndexedTile = tileIdxMap[getTileIdxByPos(newX, newY)];
-  
+
   oldIndexedTile && oldIndexedTile.removeEnt(entity);
   newIndexedTile && newIndexedTile.addEnt(entity);
 }
