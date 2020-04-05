38bf0c6f73135e1012ef7898dfdfed12810c60a1
Patrick <patrick.tolosa@gmail.com>
2020-04-05 21:11:07 +0300

    Adjust styling

--- src/ui/utils/resizeGameElements.ts
+++ src/ui/utils/resizeGameElements.ts
@@ -1,13 +1,16 @@
 function resizeGameElements(isEditing = false) {
-  let widthToHeight = 1.6666; // TODO is this magical ?
-  let editorHeight = isEditing ? 170 : 0;
-  let UIHeight = 100;
-  let newWidth = window.innerWidth;
-  let newHeight = window.innerHeight - UIHeight - editorHeight;
-  let newWidthToHeight = newWidth / newHeight;
-
   let gameArea = document.querySelector('.wrapper') as HTMLElement;
   let gameUI = document.querySelector('.game-ui') as HTMLElement;
+  let widthToHeight = 1.6666; // TODO is this magical ?
+  let editorHeight = isEditing ? 170 : 0;
+
+  let styles = window.getComputedStyle(gameUI);
+  let heightStyle = styles.getPropertyValue('height');
+
+  let gameUIHeight = +heightStyle.replace('px', '');
+  let newWidth = window.innerWidth;
+  let newHeight = window.innerHeight - gameUIHeight - editorHeight;
+  let newWidthToHeight = newWidth / newHeight;
 
   if (gameArea) {
     if (newWidthToHeight > widthToHeight) {
