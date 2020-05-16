76734869d0fe6b5903e3897e29e570b56cd76854
Patrick <patrick.tolosa@gmail.com>
2020-05-16 20:13:21 +0300

    Add a CSV file for levels database, and add a tool to parse it to JSON

---
+++ buildData.js
@@ -0,0 +1,42 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const csvFilePath = __dirname + '/src/levels/levels.csv';
+
+/**
+ * Validate:
+ * - All monsters exist by name somewhere
+ * - All level IDs have proper directories in /levels (0-0 exists, 0-1 exists etc.)
+ * - Ensure player_start_pos is not out of map
+ */
+csv()
+  .fromFile(csvFilePath)
+  .then((levels)=>{
+
+    levels.forEach((lvl) => {
+      let [x, y] = lvl.player_start_pos.split(',');
+      lvl.player_start_pos = {x, y};
+
+      lvl.monster_spawns = lvl.monster_spawns.split(',');
+      let levelExits = lvl.exits.split('__');
+
+      lvl.exits = {};
+      levelExits.forEach((exit) => {
+        let [sourcePosition, encodedTargetLevel] = exit.split('->');
+        if (!encodedTargetLevel) {
+          return;
+        }
+        let [targetLevelID, targetTilePosition] = encodedTargetLevel.split('@');
+        let [targetArea, targetLevel] = targetLevelID.split('-');
+
+        lvl.exits[sourcePosition] = {
+          area: targetArea,
+          level: targetLevel
+        }
+      })
+
+
+    });
+
+    fs.writeFileSync (__dirname + '/src/levels/levels.json', JSON.stringify(levels, '\t'));
+  });
