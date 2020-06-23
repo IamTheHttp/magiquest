6f1ffcbb79ea3358dc47eeda8fc224fe19e0be86
Patrick <patrick.tolosa@gmail.com>
2020-06-23 22:29:54 +0300

    Round down currentHealth to nearest integer in the UI

--- src/ui/Components/GameUI/GameUI.tsx
+++ src/ui/Components/GameUI/GameUI.tsx
@@ -18,7 +18,7 @@ function GameUI(props: IGameUIProps) {
       <h3>Health</h3>
       <div className='bar'>
         <div className='bar__filled bar__filled health' style={{width: `${props.percentHealth * 100}%`}}/>
-        <div className='stats'>{props.currentHealth} / {props.maxHealth}</div>
+        <div className='stats'>{Math.floor(props.currentHealth)} / {props.maxHealth}</div>
       </div>
 
       <h3>Exp</h3>
